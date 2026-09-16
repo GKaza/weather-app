376231d4d8c491dacca9e4f0d2c8301475112e87
Giannis Kazakidis <jkazakidis@gmail.com>
2026-09-17 01:53:08 +0300

    FIX FETCHING ISSUE

--- src/components/Cities.vue
+++ src/components/Cities.vue
@@ -1,141 +1,124 @@
 <template>
-  <div class="cities-container">
-    <div class="sort-buttons">
-      <p>sort:</p>
-      <button
-        class="sort-button"
-        @click="sortOrder = 'az'"
-        v-if="sortOrder !== 'az'"
-      >
-        A-Z
-      </button>
-      <button
-        class="sort-button"
-        @click="sortOrder = 'za'"
-        v-if="sortOrder !== 'za'"
-      >
-        Z-A
-      </button>
-    </div>
-    <ul class="cities-list">
-      <li v-for="city in sortCities" :key="city.id">
-        <router-link
-          class="city-link"
-          :to="{ name: 'City', params: { id: city.id } }"
-        >
-          <div class="city-button">{{ city.name }}</div>
-        </router-link>
-      </li>
-    </ul>
-  </div>
+	<div class="cities-container">
+		<div class="sort-buttons">
+			<p>sort:</p>
+			<button
+				class="sort-button"
+				@click="sortOrder = 'az'"
+				v-if="sortOrder !== 'az'"
+			>
+				A-Z
+			</button>
+			<button
+				class="sort-button"
+				@click="sortOrder = 'za'"
+				v-if="sortOrder !== 'za'"
+			>
+				Z-A
+			</button>
+		</div>
+		<ul class="cities-list">
+			<li v-for="city in sortCities" :key="city.id">
+				<router-link
+					class="city-link"
+					:to="{ name: 'City', params: { id: city.id } }"
+				>
+					<div class="city-button">{{ city.name }}</div>
+				</router-link>
+			</li>
+		</ul>
+	</div>
 </template>
 
 <script lang="ts">
-import Vue from "vue";
+import Vue from 'vue';
 
 export default Vue.extend({
-  name: "Cities",
-  data() {
-    return {
-      sortOrder: "",
-    };
-  },
-  computed: {
-    sortCities: function () {
-      if (this.sortOrder == "az") {
-        return this.$store.getters.cities.slice().sort((a: any, b: any) => {
-          const nameA = a.name.toUpperCase();
-          const nameB = b.name.toUpperCase();
-          if (nameA < nameB) {
-            return -1;
-          }
-          if (nameA > nameB) {
-            return 1;
-          }
-          return 0;
-        });
-      } else if (this.sortOrder == "za") {
-        return this.$store.getters.cities.slice().sort((a: any, b: any) => {
-          const nameA = a.name.toUpperCase();
-          const nameB = b.name.toUpperCase();
-          if (nameA > nameB) {
-            return -1;
-          }
-          if (nameA < nameB) {
-            return 1;
-          }
-          return 0;
-        });
-      } else {
-        return this.$store.getters.cities;
-      }
-    },
-    // sortCities is a computed property that returns our cities array sorted depending on the sortOrder declared in our data
-  },
+	name: 'Cities',
+	data() {
+		return {
+			sortOrder: '',
+		};
+	},
+	computed: {
+		sortCities: function () {
+			const cities = this.$store.getters.cities;
+
+			if (this.sortOrder !== 'az' && this.sortOrder !== 'za') {
+				return cities;
+			}
+
+			const direction = this.sortOrder === 'az' ? 1 : -1;
+
+			return cities
+				.slice()
+				.sort((a, b) => direction * a.name.localeCompare(b.name));
+		},
+	},
 });
 </script>
 
 <style scoped>
 .cities-list {
-  list-style-type: none;
-  display: flex;
-  flex-direction: column;
-  padding-left: 0;
-  padding-bottom: 50px;
-  width: fit-content;
-  margin: auto;
+	list-style-type: none;
+	display: flex;
+	flex-direction: column;
+	padding-left: 0;
+	padding-bottom: 50px;
+	width: fit-content;
+	margin: auto;
 }
 .cities-list li {
-  overflow: auto;
-  padding: 0.5rem;
+	overflow: auto;
+	padding: 0.5rem;
 }
 
 .city-button {
-  transition: 400ms;
-  border: none;
-  border-radius: 1vw;
-  margin: 0;
-  padding: 1rem;
-  min-width: fit-content;
-  width: 160px;
-  background-color: rgba(0, 0, 0, 0.2);
-  text-transform: uppercase;
-  letter-spacing: 1px;
+	transition: 400ms;
+	border: none;
+	border-radius: 1vw;
+	margin: 0;
+	padding: 1rem;
+	min-width: fit-content;
+	width: 160px;
+	background-color: rgba(0, 0, 0, 0.2);
+	text-transform: uppercase;
+	letter-spacing: 1px;
 }
 
 .city-button:hover {
-  background-color: rgba(0, 0, 0, 0.4);
-  letter-spacing: 2px;
+	background-color: rgba(0, 0, 0, 0.4);
+	letter-spacing: 2px;
 }
 
 .city-link {
-  text-decoration: none;
-  display: inline-block;
-  width: 100%;
-  height: 100%;
-  color: #ffffff;
+	text-decoration: none;
+	display: inline-block;
+	width: 100%;
+	height: 100%;
+	color: #ffffff;
 }
 
 .sort-buttons {
-  padding: 1rem;
+	padding: 1rem;
 }
 
 .sort-buttons p {
-  display: inline;
-  margin: 0rem 0.4rem;
+	display: inline;
+	margin: 0rem 0.4rem;
 }
 .sort-button {
-  cursor: pointer;
-  transition: 400ms;
-  color: #ffffff;
-  border: none;
-  border-radius: 0.6vw;
-  padding: 0.5rem;
-  margin: 0 0.4rem;
-  background-color: rgba(0, 0, 0, 0.1);
-  outline: none;
+	cursor: pointer;
+	transition: 400ms;
+	color: #ffffff;
+	border: none;
+	border-radius: 0.6vw;
+	padding: 0.5rem;
+	margin: 0 0.4rem;
+	background-color: rgba(0, 0, 0, 0.1);
+	outline: none;
 }
 .sort-button:hover {
-  background-color: rgba(0, 0, 0, 0.2);
+	background-color: rgba(0, 0, 0, 0.2);
 }
 </style>
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,63 +1,63 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from 'vue';
+import Vuex from 'vuex';
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-    cities: [] as any,
-    URLs: [
-      "http://api.openweathermap.org/data/2.5/weather?id=2643743&units=metric&APPID=d86265c8d6a106614454e5d281e8bf99",
-      "http://api.openweathermap.org/data/2.5/weather?id=5128581&units=metric&APPID=d86265c8d6a106614454e5d281e8bf99",
-      "http://api.openweathermap.org/data/2.5/weather?id=2968815&units=metric&APPID=d86265c8d6a106614454e5d281e8bf99",
-      "http://api.openweathermap.org/data/2.5/weather?id=5368361&units=metric&APPID=d86265c8d6a106614454e5d281e8bf99",
-      "http://api.openweathermap.org/data/2.5/weather?id=2759794&units=metric&APPID=d86265c8d6a106614454e5d281e8bf99",
-      "http://api.openweathermap.org/data/2.5/weather?id=3169070&units=metric&APPID=d86265c8d6a106614454e5d281e8bf99",
-      "http://api.openweathermap.org/data/2.5/weather?id=3117735&units=metric&APPID=d86265c8d6a106614454e5d281e8bf99",
-      "http://api.openweathermap.org/data/2.5/weather?id=1850147&units=metric&APPID=d86265c8d6a106614454e5d281e8bf99",
-      "http://api.openweathermap.org/data/2.5/weather?id=1816670&units=metric&APPID=d86265c8d6a106614454e5d281e8bf99",
-      "http://api.openweathermap.org/data/2.5/weather?id=3451190&units=metric&APPID=d86265c8d6a106614454e5d281e8bf99",
-    ],
-  },
-  mutations: {
-    SAVE_CITY(state, payload) {
-      state.cities.push(payload)
-    },
-    // SAVE_CITY mutation is used to push a city's data into our cities array 
-    CLEAR_CITIES(state) {
-      state.cities = [] as any
-    }
-    // CLEAR_CITIES makes sure our cities array is empty
-  },
-  actions: {
-    async loadCities({ commit }) {
-      commit("CLEAR_CITIES")
-      await this.state.URLs.forEach((url) =>
-        fetch(url)
-          .then((response) => response.json())
-          .then((result) => { commit('SAVE_CITY', result) })
-      );
-    }
-    // loadCities action makes GET requests to our URLs and pushes each response to the cities array
-  },
-  getters: {
-    cities(state) {
-      return state.cities
-    },
-    coldCities(state) {
-      return state.cities.filter((city: { main: { temp: number } }) => {
-        return city.main.temp < 20;
-      })
-    }
-    // we filter the original array so that we only get the cities with a temperature lower than 20 degrees 
-    ,
-    warmCities(state) {
-      return state.cities.filter((city: { main: { temp: number } }) => {
-        return city.main.temp > 20;
-      })
-    }
-    // we filter again for the cities over 20 degrees
-  },
-  modules: {
-  }
-})
+	state: {
+		cities: [] as any,
+		URLs: [
+			`https://api.openweathermap.org/data/2.5/weather?id=2643743&units=metric&APPID=${process.env.API_KEY}`,
+			`https://api.openweathermap.org/data/2.5/weather?id=5128581&units=metric&APPID=${process.env.API_KEY}`,
+			`https://api.openweathermap.org/data/2.5/weather?id=2968815&units=metric&APPID=${process.env.API_KEY}`,
+			`https://api.openweathermap.org/data/2.5/weather?id=5368361&units=metric&APPID=${process.env.API_KEY}`,
+			`https://api.openweathermap.org/data/2.5/weather?id=2759794&units=metric&APPID=${process.env.API_KEY}`,
+			`https://api.openweathermap.org/data/2.5/weather?id=3169070&units=metric&APPID=${process.env.API_KEY}`,
+			`https://api.openweathermap.org/data/2.5/weather?id=3117735&units=metric&APPID=${process.env.API_KEY}`,
+			`https://api.openweathermap.org/data/2.5/weather?id=1850147&units=metric&APPID=${process.env.API_KEY}`,
+			`https://api.openweathermap.org/data/2.5/weather?id=1816670&units=metric&APPID=${process.env.API_KEY}`,
+			`https://api.openweathermap.org/data/2.5/weather?id=3451190&units=metric&APPID=${process.env.API_KEY}`,
+		],
+	},
+	mutations: {
+		SAVE_CITY(state, payload) {
+			state.cities.push(payload);
+		},
+		// SAVE_CITY muta`ion is used to push a city's data into our cities array
+		CLEAR_CITIES(state) {
+			state.cities = [] as any;
+		},
+		// CLEAR_CITIES makes sure our cities array is empty
+	},
+	actions: {
+		async loadCities({ commit }) {
+			commit('CLEAR_CITIES');
+			await this.state.URLs.forEach((url) =>
+				fetch(url)
+					.then((response) => response.json())
+					.then((result) => {
+						commit('SAVE_CITY', result);
+					}),
+			);
+		},
+		// loadCities action makes GET requests to our URLs and pushes each response to the cities array
+	},
+	getters: {
+		cities(state) {
+			return state.cities;
+		},
+		coldCities(state) {
+			return state.cities.filter((city: { main: { temp: number } }) => {
+				return city.main.temp < 20;
+			});
+		},
+		// we filter the original array so that we only get the cities with a temperature lower than 20 degrees
+		warmCities(state) {
+			return state.cities.filter((city: { main: { temp: number } }) => {
+				return city.main.temp > 20;
+			});
+		},
+		// we filter again for the cities over 20 degrees
+	},
+	modules: {},
+});
